37a88b4b16c186e738d7b3da1ff1b3f2281611f6
Ryo ∴ SpiralArchitect <kishkavsesvit@icloud.com>
2025-10-11 05:27:37 -0700

    Update __init__.py

--- src/tiger_optim/__init__.py
+++ src/tiger_optim/__init__.py
@@ -14,6 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ====================================================================
+
 from .tiger import Tiger
 from .tagged import build_tagged_param_groups
 from .schedulers import TagWarmupDecay
